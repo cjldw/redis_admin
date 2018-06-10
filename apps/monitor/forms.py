50e9cf007cd9dea7cde9e1ad51b89861dcd3b137
luowen <bigpao.luo@gmail.com>
2018-06-10 15:40:51 +0800

    fix

--- apps/monitor/forms.py
+++ apps/monitor/forms.py
@@ -6,11 +6,11 @@ from django import forms
 # from users.models import RedisConf
 
 class RedisForm(forms.ModelForm):
-    name = forms.CharField(max_length=10, verbose_name=u"名称")
-    host = forms.CharField(max_length=15, verbose_name=u"IP地址")
-    port = forms.IntegerField(default=6379, verbose_name=u"端口")
-    password = forms.CharField(null=True, blank=True, max_length=30, verbose_name=u"密码")
-    database = forms.IntegerField(default=16, verbose_name=u"db数")
+    name = forms.CharField(max_length=10, required=True)
+    host = forms.CharField(max_length=15, required=True)
+    port = forms.IntegerField(default=6379, required=True)
+    password = forms.CharField(null=True, blank=True, max_length=30)
+    database = forms.IntegerField(default=16, required=True)
     # class Meta:
     #     model = RedisConf
     #     fields = '__all__'
